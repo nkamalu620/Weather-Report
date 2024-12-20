ec9e2a9fa5af027031d2f0ef62e2d3d3a7d0b4f1
nkamalu620 <nkamalu96@gmail.com>
2024-12-19 19:42:18 -0500

    updated code for htmlRoute.ts file

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -6,6 +6,10 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 const express = require('express');
 const path = require('path');
 
